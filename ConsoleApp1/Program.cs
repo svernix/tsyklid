0557d00fb36a6dc2f3ffdda7b44120a57c942b8e
Opilane TTHK <opilane@tthk.ee>
2025-10-27 08:38:57 +0200

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -36,28 +36,44 @@ if (kasutajanimi == "user1")
 Console.WriteLine("Tere tulemast MobiFix parandusautomaati! Kuidas saan aidata?");
 Console.WriteLine("0 - headaega\n1 - tahan telefoni parandada");
 Console.WriteLine("Palun tee oma valik, kirjutades vastav arv");
-int kasutajaTelefon = int.Parse(Console.ReadLine());
-while (kasutajaTelefon < 1 && kasutajaTelefon > 4)
+int kasutajaValik = int.Parse(Console.ReadLine());
+while (kasutajaValik < 1 && kasutajaValik > 1)
 {
-    kasutajaTelefon = int.Parse(Console.ReadLine());
     Console.WriteLine("Palun tee oma valik, kirjutades vastav arv");
+    kasutajaValik = int.Parse(Console.ReadLine());
 }
-switch (kasutajaTelefon)
+if (kasutajaValik = 0)
 {
-    case 1:
-        Console.WriteLine("Aitäh, oma iPhone saad tagasi 1 nädala pärast");
-        break;
-    case 2:
-        Console.WriteLine("Aitäh, sinu Xiaomi on valmis 2 kuu pärast");
-        break;
-    case 3:
-        Console.WriteLine("Kahjusk me huaweid ei teenunda");
-        break;
-    case 4:
-        Console.WriteLine("Sinu samsung saav valmis 2 päeva pärast");
-        break;
-    default:
-        break;
+    Console.WriteLine("Headaega, tulge jälle");
+}
+else 
+{
+    Console.WriteLine("Tere tulemast MobiFix parandusautomaati! Kuidas saan aidata?");
+    Console.WriteLine("0 - headaega\n1 - tahan telefoni parandada");
+    Console.WriteLine("Palun tee oma valik, kirjutades vastav arv");
+    int kasutajaTelefon = int.Parse(Console.ReadLine());
+    while (kasutajaTelefon < 1 && kasutajaTelefon > 1)
+    {
+        kasutajaTelefon = int.Parse(Console.ReadLine());
+        Console.WriteLine("Palun tee oma valik, kirjutades vastav arv");
+    }
+    switch
+    {
+        case 1:
+            Console.WriteLine("Aitäh, oma iPhone saad tagasi 1 nädala pärast");
+            break;
+        case 2:
+            Console.WriteLine("Aitäh, sinu Xiaomi on valmis 2 kuu pärast");
+            break;
+        case 3:
+            Console.WriteLine("Kahjusk me huaweid ei teenunda");
+            break;
+        case 4:
+            Console.WriteLine("Sinu samsung saav valmis 2 päeva pärast");
+            break;
+        default:
+            break;
+    }
 }
 Console.WriteLine("Kas soovid mõnda muud mudelit parandada");
 Console.WriteLine("o - jah\n1 - ei");
