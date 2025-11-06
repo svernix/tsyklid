1b4a85ebedcc5b1878e63b488af1faeb469ea045
Opilane TTHK <opilane@tthk.ee>
2025-11-06 10:01:42 +0200

    Update Program.cs

--- For/For/Program.cs
+++ For/For/Program.cs
@@ -8,19 +8,20 @@
 //    Console.Beep();
 //}
 
-string[] objectArray = new string[5];
-int opjectCount = 0;
-string temp = "no name";
-while (opjectCount < 5)
+string[] objectArray = new string[5]; //array, massiv mitmest elemenist
+int opjectCount = 0; //loend kui palju kordi küsida
+string temp = "no name"; //muutuja mis hetkel on "tühi"
+while (opjectCount < 5) //while tsükke mis küsib nimesid
 {
-    Console.WriteLine("Sisesta järgmine lemmikloomanimi");
-    temp = Console.ReadLine();
-    objectArray[opjectCount] =temp;
-    opjectCount++;
+    Console.WriteLine("Sisesta järgmine lemmikloomanimi"); 
+    temp = Console.ReadLine(); // temp muutujasse väärtuse (mida me tahame) sisestamine
+    objectArray[opjectCount] =temp; //masiivi, asukohal objectCount
+    opjectCount++; //tsükli inkrementatsioon
 }
-Console.WriteLine("Oled sisetanud järgnevad objektid");
+Console.WriteLine("Oled sisetanud järgnevad objektid"); //sõnum
 for (int i = 0; i < objectArray.Length; i++)
 {
     Console.WriteLine($"{i+1}. element on: {objectArray[i]}");
+    //kuvame välja reanumbri koos vastava elemendigam muutuja i abil
 }
-Console.ReadLine();
+Console.ReadLine(); // peatame programmi klahvisiestuse taha
